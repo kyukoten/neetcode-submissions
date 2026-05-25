48ab146ed5ce6dd77513c0b18de009242634d9cd
Zenith <166545612+kyukoten@users.noreply.github.com>
2026-05-25 12:59:46 +0530

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size()>s2.size())
+            return false;
+        vector<int>freq(26,0);
+        for(auto i:s1)
+            freq[i-'a']++;
+        vector<int>temp(26,0);    
+        int l=0;
+        int r=0;
+        while (r<s1.size())
+        {
+            temp[s2[r]-'a']++;
+            r++;
+        }
+        int count=0;
+        for (int i=0; i<26; i++)
+        {
+            if (freq[i]==temp[i])
+                count++;
+        }
+        if (count==26)
+            return true;
+        while (r<s2.size())
+        {
+            int a = s2[l]-'a';
+            int b = s2[r]-'a';
+            if (a==b)
+            {
+                l++;
+                r++;
+                continue;
+            }
+            temp[a]--;
+            temp[b]++;
+            if (temp[a]==freq[a]-1)
+                count--;
+            else if(temp[a]==freq[a])
+                count++;
+            if(temp[b]==freq[b]+1)
+                count--;
+            else if(temp[b]==freq[b])
+                count++;
+            if (count==26)
+                return true;
+            l++;
+            r++;    
+        }
+        return false;
+    }
+};
